23f98a4850988517d864e926f756b66e6941e5f5
Willem Melching <willem.melching@gmail.com>
2018-11-26 19:54:08 +0100

    run test using setup.py test

--- setup.py
+++ setup.py
@@ -7,30 +7,14 @@ from setuptools import setup, Extension
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-def read(*parts):
-  """Taken from pypa pip setup.py:
-  intentionally *not* adding an encoding option to open, See:
-  https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
-  """
-  return codecs.open(os.path.join(here, *parts), 'r').read()
-
-
-def find_version(*file_paths):
-  version_file = read(*file_paths)
-  version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                            version_file, re.M)
-  if version_match:
-    return version_match.group(1)
-  raise RuntimeError("Unable to find version string.")
-
 setup(
   name='laika',
-  version=find_version("laika", "__init__.py"),
+  version='0.0.1',
   url='https://github.com/commaai/laika',
   author='comma.ai',
   author_email='harald@comma.ai',
   packages=[
-    'laika',
+    'gnss',
     ],
   package_dir = {'laika': 'gnss'},
   platforms='any',
@@ -52,5 +36,7 @@ setup(
     "Programming Language :: Python :: 3",
     "Topic :: System :: Hardware",
   ],
+  setup_requires=['pytest-runner'],
+  tests_require=['pytest'],
 )
 
