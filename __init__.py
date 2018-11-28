8b6999a38964c57d224a5e6e4f913a12340ae0ac
Harald Schafer <harald.the.engineer@gmail.com>
2018-11-28 14:01:13 -0800

    better package management

---
+++ __init__.py
@@ -0,0 +1,1 @@
+from gnss import *
---
+++ gnss/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ['dgps', 'raw_gnss', 'dcb', 'iono',
+           'astro_dog', 'ephemeris', 'gps_time',
+           'helpers', 'constants', 'rinex_file',
+           'downloader']
