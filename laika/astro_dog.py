d9350765d7cdb82fbcd789aea84e6a8378ef236b
Gijs Koning <gijs-koning@live.nl>
2022-06-01 14:30:11 +0200

    Fix confusion in function names

--- laika/astro_dog.py
+++ laika/astro_dog.py
@@ -6,7 +6,7 @@ from .constants import SECS_IN_DAY
 from .helpers import ConstellationId, get_constellation, get_closest, get_el_az, TimeRangeHolder
 from .ephemeris import EphemerisType, GLONASSEphemeris, GPSEphemeris, PolyEphemeris, parse_sp3_orbits, parse_rinex_nav_msg_gps, \
   parse_rinex_nav_msg_glonass
-from .downloader import download_orbits, download_orbits_russia, download_nav, download_ionex, download_dcb
+from .downloader import download_gps_orbits, download_other_orbits, download_nav, download_ionex, download_dcb
 from .downloader import download_cors_station
 from .trop import saast
 from .iono import IonexMap, parse_ionex
@@ -165,15 +165,16 @@ class AstroDog:
 
     time_steps = [gps_time - SECS_IN_DAY, gps_time, gps_time + SECS_IN_DAY]
     with ThreadPoolExecutor() as executor:
-      if "GLONASS" in self.valid_const:
-        futures_russia = [executor.submit(download_orbits_russia, t, self.cache_dir, self.valid_ephem_types) for t in time_steps]
+      futures_other = futures_gps = None
+      if len(set(self.valid_const).difference(["GPS"])) > 0:
+        futures_other = [executor.submit(download_other_orbits, t, self.cache_dir, self.valid_ephem_types) for t in time_steps]
       if "GPS" in self.valid_const:
-        futures_orbits = [executor.submit(download_orbits, t, self.cache_dir, self.valid_ephem_types) for t in time_steps]
+        futures_gps = [executor.submit(download_gps_orbits, t, self.cache_dir, self.valid_ephem_types) for t in time_steps]
 
-      ephems_sp3_ru = parse_orbits(futures_russia) if "GLONASS" in self.valid_const else []
-      ephems_sp3_us = parse_orbits(futures_orbits) if "GPS" in self.valid_const else []
+      ephems_sp3_other = parse_orbits(futures_other) if futures_other else []
+      ephems_sp3_us = parse_orbits(futures_gps) if futures_gps else []
 
-    return ephems_sp3_ru + ephems_sp3_us
+    return ephems_sp3_other + ephems_sp3_us
 
   def get_orbit_data(self, time: GPSTime):
     ephems_sp3 = self.download_parse_orbit_data(time)
