d2288f6504c1925134ebce90e08dee72a2a02f3f
Gijs Koning <gijs-koning@live.nl>
2022-06-02 19:07:57 +0200

    Revert

--- laika/astro_dog.py
+++ laika/astro_dog.py
@@ -39,7 +39,6 @@ class AstroDog:
     self.dgps = dgps
     if not isinstance(valid_ephem_types, Iterable):
       valid_ephem_types = [valid_ephem_types]
-    # todo check that final/rapid are not combined with ultra
     self.pull_orbit = len(set(EphemerisType.observation_orbits()) & set(valid_ephem_types)) > 0
     self.pull_nav = EphemerisType.NAV in valid_ephem_types
     self.valid_const = valid_const
@@ -163,6 +162,9 @@ class AstroDog:
   def download_parse_orbit_data(self, gps_time: GPSTime, skip_before_epoch=None) -> List[PolyEphemeris]:
     # Download multiple days to be able to polyfit at the start-end of the day
     time_steps = [gps_time - SECS_IN_DAY, gps_time, gps_time + SECS_IN_DAY]
+    # Assume Ultra-rapid is only used for online data. Only download next day if we use Final or Rapid orbits.
+    if EphemerisType.ULTRA_RAPID_ORBIT in self.valid_ephem_types:
+      time_steps = [gps_time]
     with ThreadPoolExecutor() as executor:
       futures_other = [executor.submit(download_orbits_russia_src, t, self.cache_dir, self.valid_ephem_types) for t in time_steps]
       futures_gps = None
