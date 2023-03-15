5189d4048811a888889b341dd75647de3dbd5952
Bruce Wayne <harald.the.engineer@gmail.com>
2023-03-14 17:26:29 -0700

    unused import

--- laika/ephemeris.py
+++ laika/ephemeris.py
@@ -6,7 +6,7 @@ from typing import Dict, List, Optional
 
 import numpy as np
 import numpy.polynomial.polynomial as poly
-from datetime import datetime, timedelta
+from datetime import datetime
 from math import sin, cos, sqrt, fabs, atan2
 
 from .gps_time import GPSTime, utc_to_gpst
