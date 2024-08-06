9679c3a0ff56480da6ceb118047adf617071d4d8
Yassine <yassine.y10@gmail.com>
2024-08-06 15:43:26 -0700

    some renaming in the test

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -8,7 +8,7 @@ from parameterized import parameterized
 from laika.downloader import download_and_cache_file, download_file, DownloadFailed
 
 @dataclass
-class IGSFileTest:
+class TestFile:
   folder_path: str
   filename: str
   compression: str
@@ -33,15 +33,15 @@ class IGSFileTest:
 
 class TestDownloader(unittest.TestCase):
 
-  igs_files = [
-    IGSFileTest(
+  files = [
+    TestFile(
       folder_path = '2323/',
       filename = 'IGS0OPSULT_20242021800_02D_15M_ORB.SP3',
       compression= '.gz',
       file_size = 192658,
       cache_dir = '/tmp/gnss/'+'cddis_products'
       ),
-    IGSFileTest(
+    TestFile(
       folder_path = '2103/',
       filename = 'igu21034_18.sp3',
       compression = '.Z',
@@ -57,62 +57,62 @@ class TestDownloader(unittest.TestCase):
   )
 
   def setUp(self) -> None:
-    for igs_file in self.igs_files:
-      for f in [igs_file.filepath, igs_file.filepath_zipped, igs_file.filepath_attempt]:
+    for file in self.files:
+      for f in [file.filepath, file.filepath_zipped, file.filepath_attempt]:
         if os.path.exists(f):
           os.remove(f)
 
-  def _download_and_cache_file(self, url_bases, igs_file, *args, **kwargs):
+  def _download_and_cache_file(self, url_bases, file, *args, **kwargs):
     return download_and_cache_file(
-      url_bases, igs_file.folder_path, cache_dir=igs_file.cache_dir, filename=igs_file.filename, compression=igs_file.compression, *args, **kwargs
+      url_bases, file.folder_path, cache_dir=file.cache_dir, filename=file.filename, compression=file.compression, *args, **kwargs
       )
 
-  @parameterized.expand(product(url_bases, igs_files))
-  def test_all_download_protocols(self, url_base, igs_file):
-    dat = download_file(url_base, igs_file.folder_path, igs_file.filename_zipped)
-    success = dat and len(dat) == igs_file.file_size
-    self.assertTrue(success, f"{url_base + igs_file.folder_path + igs_file.filename_zipped} is incorrect size: {0 if not dat else len(dat)}")
+  @parameterized.expand(product(url_bases, files))
+  def test_all_download_protocols(self, url_base, file):
+    dat = download_file(url_base, file.folder_path, file.filename_zipped)
+    success = dat and len(dat) == file.file_size
+    self.assertTrue(success, f"{url_base + file.folder_path + file.filename_zipped} is incorrect size: {0 if not dat else len(dat)}")
 
-  @parameterized.expand(igs_files)
-  def test_download_list(self, igs_file):
-    file = self._download_and_cache_file(self.url_bases, igs_file)
-    self.assertIsNotNone(file)
+  @parameterized.expand(files)
+  def test_download_list(self, file):
+    downloaded_file = self._download_and_cache_file(self.url_bases, file)
+    self.assertIsNotNone(downloaded_file)
 
-  @parameterized.expand(igs_files)
-  def test_download_list_with_disabled_mirror(self, igs_file):
-    file = self._download_and_cache_file((None, "", self.url_bases[0]), igs_file)
-    self.assertIsNotNone(file)
+  @parameterized.expand(files)
+  def test_download_list_with_disabled_mirror(self, file):
+    downloaded_file = self._download_and_cache_file((None, "", self.url_bases[0]), file)
+    self.assertIsNotNone(downloaded_file)
 
-  @parameterized.expand(igs_files)
-  def test_download_overwrite(self, igs_file):
-    file = self._download_and_cache_file(self.url_bases, igs_file)
-    self.assertIsNotNone(file)
+  @parameterized.expand(files)
+  def test_download_overwrite(self, file):
+    downloaded_file = self._download_and_cache_file(self.url_bases, file)
+    self.assertIsNotNone(downloaded_file)
     # Should overwrite file without error
-    file = self._download_and_cache_file(self.url_bases, igs_file, overwrite=True)
-    self.assertIsNotNone(file)
+    downloaded_file = self._download_and_cache_file(self.url_bases, file, overwrite=True)
+    self.assertIsNotNone(downloaded_file)
 
-  @parameterized.expand(igs_files)
-  def test_write_attempt_file_on_error(self, igs_file):
-    self.assertFalse(os.path.exists(igs_file.filepath_attempt))
+  @parameterized.expand(files)
+  def test_write_attempt_file_on_error(self, file):
+    self.assertFalse(os.path.exists(file.filepath_attempt))
 
     with patch("laika.downloader.download_file", side_effect=DownloadFailed), self.assertRaises(DownloadFailed):
-      self._download_and_cache_file(self.url_bases, igs_file)
-      self.assertTrue(os.path.exists(igs_file.filepath_attempt), msg="Attempt file should have been written after exception")
+      self._download_and_cache_file(self.url_bases, file)
+      self.assertTrue(os.path.exists(file.filepath_attempt), msg="Attempt file should have been written after exception")
 
     # Should raise when trying again after failure
     with self.assertRaises(DownloadFailed):
-      self._download_and_cache_file(self.url_bases, igs_file)
+      self._download_and_cache_file(self.url_bases, file)
 
-  @parameterized.expand(igs_files)
-  def test_wait_after_failure(self, igs_file):
+  @parameterized.expand(files)
+  def test_wait_after_failure(self, file):
     # Verify no failure first.
-    self._download_and_cache_file(self.url_bases, igs_file)
+    self._download_and_cache_file(self.url_bases, file)
 
     # Verify last download fails due failure waiting time.
     with patch("laika.downloader.download_file", side_effect=DownloadFailed), self.assertRaises(DownloadFailed):
-      self._download_and_cache_file(self.url_bases, igs_file, overwrite=True)
+      self._download_and_cache_file(self.url_bases, file, overwrite=True)
     with self.assertRaises(DownloadFailed):
-      self._download_and_cache_file(self.url_bases, igs_file)
+      self._download_and_cache_file(self.url_bases, file)
 
 
 if __name__ == "__main__":
